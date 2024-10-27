8cbf94c18635b1224fee254a822b6dca680c0951
HoangDat47 <dat47707@gmail.com>
2024-10-27 20:51:13 +0700

    Add sign up function

--- lib/src/auth/pages/sign_up_page_view.dart
+++ lib/src/auth/pages/sign_up_page_view.dart
@@ -1,10 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:table_order/src/auth/firebase_auth_implementation/firebase_auth_services.dart';
 import 'package:table_order/src/auth/widgets/form_container_widget.dart';
 
-class SignUpPageView extends StatelessWidget {
+import '../../settings/settings_controller.dart';
+
+class SignUpPageView extends StatefulWidget {
   const SignUpPageView({super.key});
 
   static const routeName = '/signup';
+
+  @override
+  State<SignUpPageView> createState() => _SignUpPageViewState();
+}
+
+class _SignUpPageViewState extends State<SignUpPageView> {
+  final FirebaseAuthServices _auth = FirebaseAuthServices();
+
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +40,15 @@ class SignUpPageView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Sign up",style: TextStyle(fontSize: 27,fontWeight: FontWeight.bold),),
+            Text(
+              "Sign up",
+              style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+            ),
             SizedBox(
               height: 30,
             ),
             FormContainerWidget(
+              controller: _usernameController,
               hintText: "Username",
               isPasswordField: false,
             ),
@@ -28,6 +56,7 @@ class SignUpPageView extends StatelessWidget {
               height: 15,
             ),
             FormContainerWidget(
+              controller: _emailController,
               hintText: "Email",
               isPasswordField: false,
             ),
@@ -35,29 +64,25 @@ class SignUpPageView extends StatelessWidget {
               height: 15,
             ),
             FormContainerWidget(
+              controller: _passwordController,
               hintText: "Password",
               isPasswordField: true,
             ),
             SizedBox(
               height: 30,
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).pushNamed('/login');
-              },
-              child: Container(
-                width: double.infinity,
-                constraints: BoxConstraints(maxHeight: 60),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+            Container(
+              width: double.infinity,
+              constraints: BoxConstraints(maxHeight: 60),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextButton(
+                onPressed: _signUp,
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
               ),
             ),
@@ -80,5 +105,26 @@ class SignUpPageView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _signUp() async {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? user = await _auth.signUpWithEmailAndPassword(email, password);
+    if (user != null && mounted) {
+      // Sử dụng builder để đảm bảo context có quyền truy cập vào provider
+      context.read<SettingsController>().updateLoginStatus(true);
+      Navigator.of(context).pushNamed("/");
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Sign up failed. Please try again."),
+          ),
+        );
+      }
+    }
   }
 }
