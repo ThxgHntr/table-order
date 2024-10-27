d77ae6ed429727af1c494087503e055b0c9bcee5
HoangDat47 <dat47707@gmail.com>
2024-10-27 12:11:46 +0700

    Update login page view

--- lib/src/auth/pages/login_page_view.dart
+++ lib/src/auth/pages/login_page_view.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_order/src/auth/widgets/form_container_widget.dart';
 
 class LoginPageView extends StatelessWidget {
   const LoginPageView({super.key});
@@ -12,10 +12,62 @@ class LoginPageView extends StatelessWidget {
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Column(
-        children: [
-          Text("Login",style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),)
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login",style: TextStyle(fontSize: 27,fontWeight: FontWeight.bold),),
+            SizedBox(
+              height: 30,
+            ),
+            FormContainerWidget(
+              hintText: "Email",
+              isPasswordField: false,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            FormContainerWidget(
+              hintText: "Password",
+              isPasswordField: true,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed('/signup');
+              },
+              child: Container(
+                width: double.infinity,
+                constraints: BoxConstraints(maxHeight: 60),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account?"),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  },
+                  child: Text("Sign up"),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
