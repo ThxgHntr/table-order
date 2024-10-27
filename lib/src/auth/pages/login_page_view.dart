e3cfc9840502c654be3a962c1f43dcadec28f1ae
HoangDat47 <dat47707@gmail.com>
2024-10-27 20:47:59 +0700

    Add login function

--- lib/src/auth/pages/login_page_view.dart
+++ lib/src/auth/pages/login_page_view.dart
@@ -1,10 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:table_order/src/auth/widgets/form_container_widget.dart';
 
-class LoginPageView extends StatelessWidget {
+import '../../settings/settings_controller.dart';
+import '../firebase_auth_implementation/firebase_auth_services.dart';
+
+class LoginPageView extends StatefulWidget {
   const LoginPageView({super.key});
 
   static const routeName = '/login';
+
+  @override
+  State<LoginPageView> createState() => _LoginPageViewState();
+}
+
+class _LoginPageViewState extends State<LoginPageView> {
+  final FirebaseAuthServices _auth = FirebaseAuthServices();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +43,7 @@ class LoginPageView extends StatelessWidget {
               height: 30,
             ),
             FormContainerWidget(
+              controller: _emailController,
               hintText: "Email",
               isPasswordField: false,
             ),
@@ -28,29 +51,25 @@ class LoginPageView extends StatelessWidget {
               height: 15,
             ),
             FormContainerWidget(
+              controller: _passwordController,
               hintText: "Password",
               isPasswordField: true,
             ),
             SizedBox(
               height: 30,
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).pushNamed('/signup');
-              },
-              child: Container(
-                width: double.infinity,
-                constraints: BoxConstraints(maxHeight: 60),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
+            Container(
+              width: double.infinity,
+              constraints: BoxConstraints(maxHeight: 60),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextButton(
+                onPressed: _signIn,
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
               ),
             ),
@@ -70,5 +89,25 @@ class LoginPageView extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _signIn() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? user = await _auth.signInWithEmailAndPassword(email, password);
+    if (user != null && mounted) {
+      // Sử dụng builder để đảm bảo context có quyền truy cập vào provider
+      context.read<SettingsController>().updateLoginStatus(true);
+      Navigator.of(context).pushNamed("/");
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Sign in failed. Please try again."),
+          ),
+        );
+      }
+    }
   }
 }
