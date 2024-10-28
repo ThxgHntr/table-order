298616ff48c804e8d51b93aff6ec24c0d2a62533
HoangDat47 <dat47707@gmail.com>
2024-10-29 01:00:23 +0700

    Add handling exception

--- lib/src/auth/firebase_auth_implementation/firebase_auth_services.dart
+++ lib/src/auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -1,18 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:table_order/src/utils/toast_utils.dart';
 
 class FirebaseAuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<User?> signInWithEmailAndPassword(String email, String password) async {
+    if (email.isEmpty || password.isEmpty) {
+      showToast('Email and password cannot be empty.');
+      return null;
+    }
+
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
       return userCredential.user;
     } on FirebaseAuthException catch (e) {
-      throw Exception(_handleAuthException(e));
+      if (e.code == 'user-not-found') {
+        showToast('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        showToast('Wrong password provided for that user.');
+      } else {
+        showToast('An error occurred: ${e.message}');
+      }
     }
+    return null;
   }
 
+
   Future<User?> signUpWithEmailAndPassword(String email, String password, {String? username}) async {
+    if (email.isEmpty || password.isEmpty) {
+      showToast('Email and password cannot be empty.');
+      return null;
+    }
+
     try {
       UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       User? user = userCredential.user;
@@ -22,22 +41,13 @@ class FirebaseAuthServices {
       }
       return user;
     } on FirebaseAuthException catch (e) {
-      throw Exception(_handleAuthException(e));
+      if (e.code == 'email-already-in-use') {
+        showToast('The account already exists for that email.');
+      } else {
+        showToast('An error occurred: ${e.message}');
+      }
     }
+    return null;
   }
 
-  String _handleAuthException(FirebaseAuthException e) {
-    switch (e.code) {
-      case 'user-not-found':
-        return 'Không tìm thấy tài khoản với email này.';
-      case 'wrong-password':
-        return 'Mật khẩu không chính xác.';
-      case 'weak-password':
-        return 'Mật khẩu quá yếu.';
-      case 'email-already-in-use':
-        return 'Email này đã được sử dụng.';
-      default:
-        return 'Lỗi không xác định. Vui lòng thử lại.';
-    }
-  }
 }
