c216668bcfd4d05c6ab7f488d69b7ea5f35fe1b6
HoangDat47 <dat47707@gmail.com>
2024-11-17 22:03:11 +0700

    add listmap

---
+++ lib/src/views/widgets/list_map.dart
@@ -0,0 +1,23 @@
+const Map<String, Map<String, List<String>>> cityDistrictMap = {
+  'Đà Nẵng': {
+    'Sơn Trà': ['Phường An Hải Bắc', 'Phường An Hải Đông', 'Phường Mân Thái'],
+    'Ngũ Hành Sơn': ['Phường Khuê Mỹ', 'Phường Mỹ An', 'Phường Hòa Hải'],
+    'Hải Châu': ['Phường Phước Ninh', 'Phường Hòa Cường Bắc', 'Phường Nam Dương'],
+    'Liên Chiểu': ['Phường Hòa Hiệp Bắc', 'Phường Hòa Khánh Bắc'],
+    'Thanh Khê': ['Phường Thanh Khê Đông', 'Phường Chính Gián'],
+    'Cẩm Lệ': ['Phường Hòa Thọ Đông', 'Phường Khuê Trung'],
+    'Hòa Vang': ['Phường Phú Mỹ', 'Phường Hòa Phú'],
+  },
+  'Hà Nội': {
+    'Ba Đình': ['Phường Đội Cấn', 'Phường Kim Mã'],
+    'Hoàn Kiếm': ['Phường Hàng Bài', 'Phường Cửa Nam'],
+    'Đống Đa': ['Phường Quảng An', 'Phường Thịnh Quang'],
+    'Cầu Giấy': ['Phường Dịch Vọng', 'Phường Mai Dịch'],
+  },
+  'TP HCM': {
+    'Quận 1': ['Phường Bến Nghé', 'Phường Tân Định'],
+    'Quận 3': ['Phường Võ Thị Sáu', 'Phường 11'],
+    'Quận 7': ['Phường Tân Hưng', 'Phường Phú Mỹ'],
+    'Tân Bình': ['Phường 1', 'Phường 4'],
+  },
+};
