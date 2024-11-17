ddec2995e916bea22f7705ebe3f87292b888ec6b
HoangDat47 <dat47707@gmail.com>
2024-11-17 22:03:19 +0700

    add RestaurantDetailsForm

---
+++ lib/src/views/widgets/restaurant_details_form.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RestaurantDetailsForm extends StatefulWidget {
+  const RestaurantDetailsForm({super.key});
+
+  static final formKey = GlobalKey<FormState>();
+
+  @override
+  State<StatefulWidget> createState() => _RestaurantDetailsFormState();
+}
+
+class _RestaurantDetailsFormState extends State<RestaurantDetailsForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
