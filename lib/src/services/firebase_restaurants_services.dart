79f2ae127d12a6accb99aea22f496426756784ba
trinhdamhuy <trinhdamhuy@gmail.com>
2024-11-24 04:18:57 +0700

    Refactor Firebase services to use Firestore instead of Realtime Database

--- lib/src/services/firebase_restaurants_services.dart
+++ lib/src/services/firebase_restaurants_services.dart
@@ -1,19 +1,21 @@
 import 'dart:io';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-
-import '../model/restaurant.dart';
+import '../model/restaurant_model.dart';
 
 class FirebaseRestaurantsServices {
   final FirebaseStorage _storage = FirebaseStorage.instance;
-  final FirebaseDatabase _database = FirebaseDatabase.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Upload image to Firebase Storage with restaurantId in the path
-  Future<String?> uploadImageToFirebaseStorage(String restaurantId, File image) async {
+  Future<String?> uploadImagetoFirestoreStorage(
+      String restaurantId, File image) async {
     try {
-      final fileName = '${restaurantId}_${DateTime.now().millisecondsSinceEpoch}_${image.path.split('/').last}';
-      final ref = _storage.ref().child('restaurant_pictures/$restaurantId/$fileName');
+      final fileName =
+          '${restaurantId}_${DateTime.now().millisecondsSinceEpoch}_${image.path.split('/').last}';
+      final ref =
+          _storage.ref().child('restaurant_pictures/$restaurantId/$fileName');
       final uploadTask = ref.putFile(image);
       final snapshot = await uploadTask.whenComplete(() => {});
       final downloadUrl = await snapshot.ref.getDownloadURL();
@@ -30,13 +32,14 @@ class FirebaseRestaurantsServices {
   }
 
   // Upload multiple images with restaurantId
-  Future<List<String>> uploadImages(String restaurantId, List<File> images) async {
+  Future<List<String>> uploadImages(
+      String restaurantId, List<File> images) async {
     List<String> downloadUrls = [];
     for (var image in images) {
       if (kDebugMode) {
         print('Uploading image: ${image.path}');
       }
-      final imageUrl = await uploadImageToFirebaseStorage(restaurantId, image);
+      final imageUrl = await uploadImagetoFirestoreStorage(restaurantId, image);
       if (imageUrl != null) {
         downloadUrls.add(imageUrl);
       } else {
@@ -48,28 +51,29 @@ class FirebaseRestaurantsServices {
     return downloadUrls;
   }
 
-  // Save restaurant info to Firebase Realtime Database, including uploaded image URLs
-  Future<bool> saveRestaurantInfo(Restaurant restaurant) async {
+  // Save restaurant info to Firestore, including uploaded image URLs
+  Future<bool> saveRestaurantInfo(RestaurantModel restaurant) async {
     try {
-      if (restaurant.selectedImage.isEmpty) {
+      if (restaurant.photos.isEmpty) {
         if (kDebugMode) {
           print('No images selected. Cannot save restaurant info.');
         }
-        return false;  // Return early if no images are selected
+        return false; // Return early if no images are selected
       }
 
-      final imageUrls = await uploadImages(
-          restaurant.restaurantId, restaurant.selectedImage.map((path) => File(path)).toList());
+      // Save the restaurant info to Firestore
+      final restaurantRef = _firestore.collection('restaurants').doc();
+      await restaurantRef.set(restaurant.toFirestore());
+
+      final imageUrls = await uploadImages(restaurant.restaurantId,
+          restaurant.photos.map((path) => File(path)).toList());
       if (imageUrls.isEmpty) {
         if (kDebugMode) {
           print('No images uploaded. Cannot save restaurant info.');
         }
-        return false;  // Return early if image upload fails
+        return false; // Return early if image upload fails
       }
-      restaurant.selectedImage = imageUrls;
-
-      final ref = _database.ref().child('restaurants').push();
-      await ref.set(restaurant.toMap());
+      restaurant.photos = imageUrls;
 
       if (kDebugMode) {
         print('Restaurant info saved successfully.');
@@ -82,5 +86,4 @@ class FirebaseRestaurantsServices {
       return false;
     }
   }
-
 }
