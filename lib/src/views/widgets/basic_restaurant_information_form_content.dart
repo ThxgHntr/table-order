bfb1b65bd9aa2782d7c5598a7039d448b475b965
trinhdamhuy <trinhdamhuy@gmail.com>
2024-11-24 22:54:23 +0700

    Add current location retrieval to restaurant address form with loading indicator

--- lib/src/views/widgets/basic_restaurant_information_form_content.dart
+++ lib/src/views/widgets/basic_restaurant_information_form_content.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:table_order/src/utils/location_helper.dart';
 
 class BasicRestaurantInformationFormContent extends StatefulWidget {
   const BasicRestaurantInformationFormContent({
@@ -22,6 +25,7 @@ class BasicRestaurantInformationFormContentState
   String? selectedCity;
   String? selectedDistrict;
   String? selectedWard;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -31,10 +35,28 @@ class BasicRestaurantInformationFormContentState
     selectedWard = null;
   }
 
+  Future<void> _getCurrentAddress() async {
+    setState(() {
+      _isLoading = true;
+    });
+    Position? currentPosition = await getCurrentLocation();
+    if (currentPosition != null) {
+      String address = await getAddressFromGeopoint(
+          GeoPoint(currentPosition.latitude, currentPosition.longitude));
+      setState(() {
+        widget.restaurantAddress.text = address;
+      });
+    }
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: const BoxConstraints(maxWidth: 300),
+      padding: const EdgeInsets.all(16.0),
+      constraints: const BoxConstraints(maxWidth: 600),
       child: Form(
         key: BasicRestaurantInformationFormContent.formKey,
         child: Column(
@@ -60,24 +82,47 @@ class BasicRestaurantInformationFormContentState
                 ),
               ),
             ),
-            TextFormField(
-              controller: widget.restaurantAddress,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Làm ơn nhập địa chỉ';
-                }
-                return null;
-              },
-              decoration: const InputDecoration(
-                labelText: 'Địa chỉ',
-                labelStyle: TextStyle(color: Colors.grey),
-                floatingLabelStyle: TextStyle(color: Colors.blue),
-                hintText: 'Nhập địa chỉ',
-                hintStyle: TextStyle(color: Colors.grey),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      TextFormField(
+                        controller: widget.restaurantAddress,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Làm ơn nhập địa chỉ';
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Địa chỉ',
+                          labelStyle: TextStyle(color: Colors.grey),
+                          floatingLabelStyle: TextStyle(color: Colors.blue),
+                          hintText: 'Nhập địa chỉ',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                        ),
+                        enabled: !_isLoading,
+                      ),
+                      if (_isLoading)
+                        const SizedBox(
+                          width: 16,
+                          height: 16,
+                          child: CircularProgressIndicator(strokeWidth: 2.0),
+                        ),
+                    ],
+                  ),
                 ),
-              ),
+                IconButton(
+                  icon: const Icon(Icons.location_pin),
+                  onPressed: _getCurrentAddress,
+                ),
+              ],
             ),
           ],
         ),
